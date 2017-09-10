c29a0b4cb18dc2c0f08caab1cfe31c3273694d8c
wenlujon <wenlujon@163.com>
2017-09-10 08:10:59 +0800

    更改导航条颜色方案

--- wp-content/themes/circday-home/functions.php
+++ wp-content/themes/circday-home/functions.php
@@ -238,7 +238,7 @@ function twentyfourteen_scripts() {
 	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.0.3' );
 
 	// Load our main stylesheet.
-	wp_enqueue_style( 'twentyfourteen-style', get_stylesheet_uri(), array(), '1.2.2' );
+	wp_enqueue_style( 'twentyfourteen-style', get_stylesheet_uri(), array(), '1.2.3' );
 
 	// Load the Internet Explorer specific stylesheet.
 	wp_enqueue_style( 'twentyfourteen-ie', get_template_directory_uri() . '/css/ie.css', array( 'twentyfourteen-style' ), '20131205' );
